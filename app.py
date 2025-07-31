63b6aa2e5357a465ab259f659655a3149251a839
Daniel Thompson <danielthompson@daniels-air-58.mynetworksettings.com>
2025-07-31 13:22:31 -0400

    buy and sell buttons integration

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('portfolio.html')
 
+@app.route("/buy", methods=["GET"])
+def buy():
+    return render_template('buy.html')
+
+@app.route("/sell", methods=["GET"])
+def sell():
+    return render_template('sell.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='127.0.0.1', port=5001)
