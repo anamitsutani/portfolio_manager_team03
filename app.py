3681a89b404b3c0cf925507a53886257ea5bc227
audrey vargas <audrey.vargas.314@gmail.com>
2025-08-01 19:20:28 +0000

    implemented accurate daily gain feature

--- app.py
+++ app.py
@@ -44,13 +44,49 @@ def get_holdings(tickers):
 def get_portfolio_value(holdings):
     return sum(ticker.get('TotalValue') for ticker in holdings)
 
+def calc_daily_gain(holdings):
+    # get yesterday's closing prices of currently-held assets
+    tickers = [h['Ticker'] for h in holdings]
+    ticker_str = " ".join(tickers)
+    
+    data = yf.download(ticker_str, period = '2d', interval = '1d', group_by = 'ticker')
+    prev_prices = {}
+    for h in holdings:
+        t = h['Ticker']
+        
+        prev_price = data[(t, 'Close')].iloc[0]
+        prev_prices[t] = prev_price
+        
+    # calculate daily gain/loss per asset type
+    prev_portfolio_val = 0
+    total_gain = 0
+    
+    for h in holdings:
+        ticker = h['Ticker']
+        amount = h['TotalAmount']
+        
+        prev_price = prev_prices[ticker]
+        curr_price = h['CurrPrice']
+        
+        prev_value = amount * prev_price
+        curr_value = amount * curr_price
+        
+        gain = curr_value - prev_value
+        prev_portfolio_val += prev_value
+        total_gain += gain
+        
+    gain_percent = (total_gain / prev_portfolio_val) * 100
+    
+    return total_gain, gain_percent
+
 @app.route("/", methods=["GET"])
 def index():
     conn, cursor = start_conn()
     tickers = get_amount_by_ticker(cursor)
     holdings = get_holdings(tickers)
+    daily_gain, gain_percent = calc_daily_gain(holdings)
     close_conn(conn)
-    return render_template('portfolio.html', holdings=holdings, current_value=get_portfolio_value(holdings))
+    return render_template('portfolio.html', holdings=holdings, current_value=get_portfolio_value(holdings), daily_gain=daily_gain, gain_percent=gain_percent)
 
 @app.route("/buy", methods=["GET"])
 def buy():
